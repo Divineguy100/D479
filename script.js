ac833fafead33e17e38d96e4451e04aa610f85c2
arog522 <arog522@wgu.edu>
2025-12-30 16:27:41 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function validateEmailMatch() {
+    const email = document.getElementById("email").value;
+    const confirmEmail = document.getElementById("confirmEmail").value;
+
+    if (email !== confirmEmail) {
+        alert("Email addresses do not match. Please try again.");
+        return false;
+    }
+    else {
+        return true;
+    }
+
+}
